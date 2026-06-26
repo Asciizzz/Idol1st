6c7038a2eb4af53b924bf2789531fcf16ffbca99
Trackmaker1742 <huutu1612@gmail.com>
2026-06-27 02:29:15 +0700

    API Implementation step 2

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,8 +4,14 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 
+use App\Models\Project;
+use App\Policies\ProjectPolicy;
+use Illuminate\Support\Facades\Gate;
+
 class AppServiceProvider extends ServiceProvider
 {
     public function register(): void {}
-    public function boot(): void {}
+    public function boot(): void {
+        Gate::policy(Project::class, ProjectPolicy::class);
+    }
 }
